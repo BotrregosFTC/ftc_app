56d09d6c039f21f6ee3d4ad44c3d8d5032cbbb88
saul-hdez <saul.omar55@gmail.com>
2016-10-29 18:00:59 -0600

    updated the description for both the MecanumDriveTrain and MecanumTankDriveTrain

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotTeleopTank_Iterative.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotTeleopTank_Iterative.java
@@ -1,23 +1,17 @@
 /*
 Copyright (c) 2016 Robert Atkinson
-
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without modification,
 are permitted (subject to the limitations in the disclaimer below) provided that
 the following conditions are met:
-
 Redistributions of source code must retain the above copyright notice, this list
 of conditions and the following disclaimer.
-
 Redistributions in binary form must reproduce the above copyright notice, this
 list of conditions and the following disclaimer in the documentation and/or
 other materials provided with the distribution.
-
 Neither the name of Robert Atkinson nor the names of his contributors may be used to
 endorse or promote products derived from this software without specific prior
 written permission.
-
 NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
 LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -35,6 +29,9 @@ package org.firstinspires.ftc.robotcontroller.external.samples;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.robot.Robot;
 import com.qualcomm.robotcore.util.Range;
 
 /**
@@ -45,40 +42,41 @@ import com.qualcomm.robotcore.util.Range;
  * All device access is managed through the HardwarePushbot class.
  *
  * This particular OpMode executes a basic Tank Drive Teleop for a PushBot
- * It raises and lowers the claw using the Gamepad Y and A buttons respectively.
+ * It raises and lowers the claw using the Gampad Y and A buttons respectively.
  * It also opens and closes the claws slowly using the left and right Bumper buttons.
  *
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-*/
+
+@TeleOp(name="Pushbot: Teleop Tank", group="Pushbot")
+@Disabled
+public class PushbotTeleopTank_Iterative extends OpMode{
+
+    /* Declare OpMode members. */
+    HardwarePushbot robot       = new HardwarePushbot(); // use the class created to define a Pushbot's hardware
+    // could also use HardwarePushbotMatrix class.
+    double          clawOffset  = 0.0 ;                  // Servo mid position
+    final double    CLAW_SPEED  = 0.02 ;                 // sets rate to move servo
+
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
     @Override
     public void init() {
         /* Initialize the hardware variables.
          * The init() method of the hardware class does all the work here
          */
         robot.init(hardwareMap);
-        @TeleOp(name="Pushbot: Teleop Tank", group="Pushbot")
-        @Disabled
-        public class PushbotTeleopTank_Iterative extends OpMode{
 
-            /* Declare OpMode members. */
-            HardwarePushbot robot       = new HardwarePushbot(); // use the class created to define a Pushbot's hardware
-            // could also use HardwarePushbotMatrix class.
-            double          clawOffset  = 0.0 ;                  // Servo mid position
-            final double    CLAW_SPEED  = 0.02 ;                 // sets rate to move servo
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+    }
 
-
-            /*
-             * Code to run ONCE when the driver hits INIT
-
-                // Send telemetry message to signify robot waiting;
-                telemetry.addData("Say", "Hello Driver");    //
-            }
-
-            /*
-             * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-             */
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
     @Override
     public void init_loop() {
     }
